86d8b1a443ec223952637a30e30e9f45b14da6ee
amit.chaudhary.jira <amch@2017>
2021-05-03 16:06:18 +0530

    new build

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -8,7 +8,7 @@ export const Todo = (props: any) => {
   return (
     <div className="item">
       <div><span>Job Id: </span>{props.data.jobId}</div>
-      <div><span>Job Title: </span><Link to={'/job?id=' + props.data.jobId }>{props.data.jobTitle}</Link></div>
+      <div><span>Job Title: </span><Link to={'/host-project/job?id=' + props.data.jobId }>{props.data.jobTitle}</Link></div>
       <div><span>Job Location: </span>{props.data.jobLocation}</div>
       <div><span>Job Description: </span>{props.data.jobDescription}</div>
       <br></br>
