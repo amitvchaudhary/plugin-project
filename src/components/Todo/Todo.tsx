0e8b7b896feac4a36ca3227bca5c703e43e678db
amit.chaudhary.jira <amch@2017>
2021-05-04 11:26:05 +0530

    new build

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Generic, JSONLD } from "react-structured-data";
 import "./Todo.css";
 
 export const Todo = (props: any) => {
   console.log('each todo ===');
   console.log(props);
   return (
+    <>
     <div className="item">
       <div><span>Job Id: </span>{props.data.jobId}</div>
       <div><span>Job Title: </span><Link to={'/host-project/job?id=' + props.data.jobId }>{props.data.jobTitle}</Link></div>
@@ -13,5 +15,37 @@ export const Todo = (props: any) => {
       <div><span>Job Description: </span>{props.data.jobDescription}</div>
       <br></br>
     </div>
+    <div>
+    <JSONLD>
+          <Generic
+            type="jobPosting"
+            jsonldtype="JobPosting"
+            schema={{
+              title: props.data.jobTitle,
+              baseSalary: "500000",
+              jobBenefits: "Life, Dental",
+              datePosted: "2021-05-03",
+              description: props.data.jobDescription,
+              educationRequirements: "Bachelor's Degree in Computer Science, Information Systems or related fields of study.",
+              employmentType: "Full-time",
+              industry: "Computer Software",
+              workHours: "60 hours per week",
+            }}
+          >
+            <Generic type="jobLocation" jsonldtype="Place">
+              <Generic
+                type="address"
+                jsonldtype="PostalAddress"
+                schema={{
+                  addressLocality: "Seattle",
+                  addressRegion: "WA",
+                }}
+              />
+            </Generic>
+          </Generic>
+        </JSONLD>
+       
+    </div>
+    </>
   );
 }
