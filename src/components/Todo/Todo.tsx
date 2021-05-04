40565e2dc6d1a2abf510af9b30d500b51be7095a
amit.chaudhary.jira <amch@2017>
2021-05-04 13:51:41 +0530

    new build

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -4,48 +4,76 @@ import { Generic, JSONLD } from "react-structured-data";
 import "./Todo.css";
 
 export const Todo = (props: any) => {
-  console.log('each todo ===');
+  console.log("each todo ===");
   console.log(props);
   return (
     <>
-    <div className="item">
-      <div><span>Job Id: </span>{props.data.jobId}</div>
-      <div><span>Job Title: </span><Link to={'/host-project/job?id=' + props.data.jobId }>{props.data.jobTitle}</Link></div>
-      <div><span>Job Location: </span>{props.data.jobLocation}</div>
-      <div><span>Job Description: </span>{props.data.jobDescription}</div>
-      <br></br>
-    </div>
-    <div>
-    <JSONLD>
+      <div className="item">
+        <div>
+          <span>Job Id: </span>
+          {props.data.jobId}
+        </div>
+        <div>
+          <span>Job Title: </span>
+          <Link to={"/host-project/job?id=" + props.data.jobId}>
+            {props.data.jobTitle}
+          </Link>
+        </div>
+        <div>
+          <span>Job Location: </span>
+          {props.data.jobLocation}
+        </div>
+        <div>
+          <span>Job Description: </span>
+          {props.data.jobDescription}
+        </div>
+        <br></br>
+      </div>
+      <div>
+        <JSONLD>
           <Generic
             type="jobPosting"
             jsonldtype="JobPosting"
             schema={{
               title: props.data.jobTitle,
-              baseSalary: "500000",
               jobBenefits: "Life, Dental",
               datePosted: "2021-05-03",
+              validThrough: "2021-06-03T12:00",
               description: props.data.jobDescription,
-              educationRequirements: "Bachelor's Degree in Computer Science, Information Systems or related fields of study.",
+              // educationRequirements: "Bachelor's Degree in Computer Science, Information Systems or related fields of study.",
               employmentType: "Full-time",
               industry: "Computer Software",
               workHours: "60 hours per week",
             }}
           >
+            <Generic
+              type="hiringOrganization"
+              jsonldtype="Organization"
+              schema={{
+                name: "Host Project",
+                sameAs: "https://amitvchaudhary.github.io/host-project/",
+                logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+              }}
+            ></Generic>
             <Generic type="jobLocation" jsonldtype="Place">
               <Generic
                 type="address"
                 jsonldtype="PostalAddress"
                 schema={{
-                  addressLocality: "Seattle",
-                  addressRegion: "WA",
+                  streetAddress: "1600 Amphitheatre Pkwy",
+                  addressLocality: "Mountain View",
+                  addressRegion: "CA",
+                  postalCode: "94043",
+                  addressCountry: "US"
                 }}
               />
             </Generic>
+            <Generic type="baseSalary" jsonldtype="MonetaryAmount" schema={{currency: "USD"}}>
+              <Generic type="value" jsonldtype="QuantitativeValue" schema={{value: "40.00", unitText: "HOUR"}}></Generic>
+            </Generic>
           </Generic>
         </JSONLD>
-       
-    </div>
+      </div>
     </>
   );
-}
+};
